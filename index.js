2faafde3a179ada7c11ec49709ca8f377cc05202
lil-ds <lilyshen@uw.edu>
2023-05-17 11:07:14 -0700

    adding after map

--- index.js
+++ index.js
@@ -84,6 +84,74 @@ mapboxgl.accessToken = 'pk.eyJ1IjoiZGltZW50aW8iLCJhIjoiY2xhMngzZmEyMDRtdDN2bW93M
         zoom: 8
     });
 
+    afterMap.on('load', () => {
+        afterMap.addSource('2020', {
+        type: 'geojson',
+        data: 'assets/2020joined.geojson'
+    });
+
+    afterMap.addLayer({
+        'id': 'after',
+        'type': 'fill',
+        'source': '2020',
+        'paint': {
+            'fill-color': [
+                'step',
+                ['coalesce', ['to-number', ['get', 'joinednewfinal_total']], 0],
+                '#FFEDA0',   // stop_output_0
+                5,          // stop_input_0
+                '#FED976',   // stop_output_1
+                10,          // stop_input_1
+                '#FEB24C',   // stop_output_2
+                20,          // stop_input_2
+                '#FD8D3C',   // stop_output_3
+                40,         // stop_input_3
+                '#FC4E2A',   // stop_output_4
+                60,         // stop_input_4
+                // '#E31A1C',   // stop_output_5
+                // 2000,         // stop_input_5
+                // '#BD0026',   // stop_output_6
+                // 5000,        // stop_input_6
+                "#800026"
+            ],
+            'fill-outline-color': '#BBBBBB',
+            'fill-opacity': 0.7,
+        }
+    });
+
+    const layers = [
+        '0-4',
+        '5-9',
+        '10-19',
+        '20-39',
+        '40-59',
+        '60+'
+    ];
+    const colors = [
+        '#FED976',
+        '#FEB24C',
+        '#FD8D3C',
+        '#FC4E2A',
+        '#E31A1C',
+        '#800026'
+    ];
+    
+    const legend = document.getElementById('legend');
+    legend.innerHTML = "<b>Population of<br> demographic</b><br><b>()</b>";
+    layers.forEach((layer, i) => {
+        const color = colors[i];
+        const item = document.createElement('div');
+        const key = document.createElement('span');
+        key.className = 'legend-key';
+        key.style.backgroundColor = color;
+    
+        const value = document.createElement('span');
+        value.innerHTML = `${layer}`;
+        item.appendChild(key);
+        item.appendChild(value);
+        legend.appendChild(item);
+    });
+})
     // A selector or reference to HTML element
     const container = '#comparison-container';
 
