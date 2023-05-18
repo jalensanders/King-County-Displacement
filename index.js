83ed373d280519d3c17d8830911ce8c6d46cdca3
kyleung1 <92060005+kyleung1@users.noreply.github.com>
2023-05-17 21:44:50 -0700

    toggle for beforemap

--- index.js
+++ index.js
@@ -1,3 +1,32 @@
+// function to toggle data
+let variable = '';
+function handleSwitch(filterVar) {
+    variable = filterVar;
+    console.log(variable)
+    // setFilter(variable);
+
+
+    beforeMap.setPaintProperty("before", 'fill-color', [
+        'step',
+        ['to-number', ['get', variable]],
+        '#FFEDA0',   // stop_output_0
+        5,          // stop_input_0
+        '#FED976',   // stop_output_1
+        10,          // stop_input_1
+        '#FEB24C',   // stop_output_2
+        20,          // stop_input_2
+        '#FD8D3C',   // stop_output_3
+        40,         // stop_input_3
+        '#FC4E2A',   // stop_output_4
+        60,         // stop_input_4
+        // '#E31A1C',   // stop_output_5
+        // 2000,         // stop_input_5
+        // '#BD0026',   // stop_output_6
+        // 5000,        // stop_input_6
+        "#800026"
+    ]);
+};
+
 mapboxgl.accessToken = 'pk.eyJ1IjoiZGltZW50aW8iLCJhIjoiY2xhMngzZmEyMDRtdDN2bW93MjYyY2hvbSJ9.lBP2u-C8BEgug7_ye16y2g';
     const beforeMap = new mapboxgl.Map({
         container: 'before',
